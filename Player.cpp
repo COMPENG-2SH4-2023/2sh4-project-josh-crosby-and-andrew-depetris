4fc2aa549fa1f52792dbc9a1570758ffa5174fe2
Josh Crosby <crosbj3@mcmaster.ca>
2023-11-27 16:26:43 -0500

    small changes

--- Player.cpp
+++ Player.cpp
@@ -9,7 +9,6 @@ Player::Player(GameMechs* thisGMRef)
     // more actions to be included
 
     playerPos.setObjPos(mainGameMechsRef->getBoardSizeX() / 2,mainGameMechsRef->getBoardSizeY() / 2 ,'@');
-    playerPos.setObjPos(5,10,'@');
 
 }
 
